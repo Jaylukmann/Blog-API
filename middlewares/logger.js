8bd032a55b1a3fa23881f6e232bbea370bb362be
Jimoh Lukman Adeyemi <jaylukmann@gmail.com>
2026-09-06 14:14:04 +0100

    Restructured folders

--- middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-
-const logger = (req,res,next) => {
-  const timestamp = new Date().toISOString();
-  console.log(`A ${req.method} request has been made on ${req.url} at ${timestamp} from ${req.ip}`);
-next();
-}; 
-
-module.exports =  logger ;
